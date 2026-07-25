6db6aa09ce9110550afef0e12b27cbfe21cb2537
Ahmed Allam <ahmed.allam@izam.com>
2026-07-25 17:40:40 +0300

    List

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+// DataType variableName ;                      Decleration
+
+// DataType variableName  = value;             Initialization
+  List<String> nameList = ["Ahmed", "Mohamed", "Alaa"];
+  nameList[0] = "Mostafa";
+  nameList.insert(0,"Ahmed");
+  print(nameList);
+}
