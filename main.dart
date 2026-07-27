c013bbdf74292d5d4b060689416e3785cebd3fe0
Ahmed Allam <ahmed.allam@izam.com>
2026-07-27 18:14:58 +0300

    var Keyword

--- main.dart
+++ main.dart
@@ -1,31 +1,8 @@
 void main() {
-// DataType variableName ;                      Decleration
-  List<Car> forEachList = [];
-// DataType variableName  = value;
-  List<int> myList = [2, 5, 56];
+  //DataType variableName = value;
+  String name = "Ahmed";
+  var myName = "Mohamed";
 
-  myList.forEach((e) => e*2);
-  List<int> newList = myList.map((e) => e * 2).toList();
-  print(newList);
-  // Map<String, dynamic> myMap = {"id": 1, "Price": 200};
-  // myMap.forEach((key, value) {
-  //   print("Value is $value");
-  // });
-  // myMap.map((key, value) => MapEntry(key, value));
+  myName = "Ahmed";
+  // Wrong myName = 1234;
 }
-
-class Car {
-  int? id;
-  String? name;
-  Car.fromJson(Map json) {
-    id = json["id"];
-    name = json["name"];
-  }
-}
-
-var jsonFromBackend = [
-  {"id": 12, "name": "BMI"},
-  {"id": 12, "name": "GG"},
-  {"id": 12, "name": "HH"},
-  {"id": 12, "name": "YY"},
-];
