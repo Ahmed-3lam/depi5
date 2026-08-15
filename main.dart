3cad3288838c7e68a07e427a1b690447b70e2058
Ahmed Allam <ahmed.allam@izam.com>
2026-08-15 17:33:39 +0300

    extension

--- main.dart
+++ main.dart
@@ -1,19 +1,14 @@
-import 'animal.dart';
-import 'car.dart';
-import 'network_helper.dart';
-import 'post_model.dart';
-import 'product.dart';
+import 'string_extension.dart';
 
 void main() {
-  Dog dog1 = Dog("Dog1");
-  Lion lion1 = Lion("Lion1");
-  lion1.eat();
-  print("=============");
-  dog1.eat();
+  String name = "Ahmed";
+  String email = "3lam.ahmeddgmail.com";
+
+  print(name.isEmailValid());
+  print(email.isEmailValid());
 }
 
+/// Future
+/// Extension
+/// Enum 
 
-/// (1) Encapsulation
-/// (2) Inheritance
-/// (3) Abstraction
-/// (4) Polymorphism
---
+++ string_extension.dart
@@ -0,0 +1,9 @@
+extension EmailValidation on String {
+  bool isEmailValid() {
+    if (this.contains("@")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
